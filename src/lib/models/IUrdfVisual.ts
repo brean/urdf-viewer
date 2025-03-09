5ad76938be033989dddd054024f526a7174ec7fa
Andreas Bresser <self@andreasbresser.de>
2025-03-09 22:38:02 +0100

    add more settings from the wiki

--- src/lib/models/IUrdfVisual.ts
+++ src/lib/models/IUrdfVisual.ts
@@ -3,12 +3,18 @@ import type IUrdfCylinder from "./IUrdfCylinder";
 import type IUrdfMesh from "./IUrdfMesh";
 
 export default interface IUrdfVisual {
+  name: string
   origin_xyz: [x: number, y: number, z: number]
   origin_rpy: [roll: number, pitch: number, yaw: number]
-  geometry: IUrdfBox | IUrdfCylinder | IUrdfMesh;
+  geometry: IUrdfBox | IUrdfCylinder | IUrdfMesh
+  material?: {
+    name: string
+    color?: any
+    texture?: string
+  }
   type: 'box' | 'cylinder' | 'mesh'
   // material color
-  color_rgba?: [r: number, g: number, b: number, a: number];
+  color_rgba?: [r: number, g: number, b: number, a: number]
   // XML Element
-  elem: Element;
+  elem: Element
 }
